709109e5f2041f9a65c1aa15a1b5c75ae546b5f6
hsanfilippo <henrique.sanfilippo@gmail.com>
2024-08-12 12:25:22 +0900

    entrega do exercicio

--- async_call/async_call/urls.py
+++ async_call/async_call/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
+   path("admin/", admin.site.urls),
+   path("", views.home_sync_view),
+   path("exercicio/", views.exercicio_view),
 ]
+
